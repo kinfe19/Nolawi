336b1989509b5eaf42c661e5959c03eac45e8b70
Esubalew Gosaye <mesaye2010@gmail.com>
2022-08-02 16:03:48 +0300

    venv and additional

--- Ncrud/urls.py
+++ Ncrud/urls.py
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
 
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-
-=======
-from django.contrib import admin
-from django.urls import path,include
-
-
-
->>>>>>> 9a77cd3ddc9b0a96b9bd2dce892d70a267cf4cdb
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('', include('crudapp.urls'), name='crudapp'),
-=======
+
     path('', include('crudapp.urls')),
 
->>>>>>> 9a77cd3ddc9b0a96b9bd2dce892d70a267cf4cdb
 ]
