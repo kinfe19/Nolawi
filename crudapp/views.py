5455f6c3476dce74906262de93f6c21af2ede20e
Esubalew Gosaye <mesaye2010@gmail.com>
2022-08-02 15:56:37 +0300

    fix manually pulling error

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 
 
-<<<<<<< HEAD
-# Create your views here.
-
 def hello(request):
     return render(request, 'crudapp/hello.html', {'name': 'World'})
-=======
+
+
 def index(request):
-	return render(request, 'index.html')
->>>>>>> 9a77cd3ddc9b0a96b9bd2dce892d70a267cf4cdb
+    return render(request, 'index.html')
