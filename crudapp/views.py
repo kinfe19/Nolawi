1bf0b51fd4e64f6a703471c55a729830f2ce5f9c
esubalew-gosaye <mesaye2010@gmail.com>
2022-08-03 06:07:30 +0300

    take some ones code

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Task
 from .forms import TaskCreationForm
-# def hello(request):
-#     return render(request, 'crudapp/hello.html', {'name': 'World'})
+
+def hello(request):
+    return render(request, 'crudapp/hello.html', {'name': 'World'})
 
 
 def index(request):
@@ -14,25 +15,25 @@ def index(request):
             return redirect("/")
     context ={"form":form}
     return render(request, 'crudapp/hello.html',context)
-# def update(request,pk):
-#     active = Task.objects.get(id=pk)
-#     form = TaskCreationForm(request.POST,instance=active)
-#     if form.is_valid():
-#                form.save()
-#                return redirect("/")
-#     context ={"form":form}
-#     return render(request, 'update.html',context)
-# def delete(request,pk):
-#     active = Task.objects.get(id=pk)
-#     if request.method =="POST":
-#         active.delete()
-#         return redirect("/")
+def update(request,pk):
+    active = Task.objects.get(id=pk)
+    form = TaskCreationForm(request.POST,instance=active)
+    if form.is_valid():
+               form.save()
+               return redirect("/")
+    context ={"form":form}
+    return render(request, 'update.html',context)
+def delete(request,pk):
+    active = Task.objects.get(id=pk)
+    if request.method =="POST":
+        active.delete()
+        return redirect("/")
 
-#     context ={"active":active}
-#     return redirect (request ,'delete.html',context)
-# def retrive(requets):
-#     task = Task.objects.all()
-#     return redirect (request , 'list.html',task)
+    context ={"active":active}
+    return redirect (request ,'delete.html',context)
+def retrive(requets):
+    task = Task.objects.all()
+    return redirect (request , 'list.html',task)
 
-# def detail(request ,pk):
-#     active_detail = Task.objects.get(id=pk)
+def detail(request ,pk):
+    active_detail = Task.objects.get(id=pk)
