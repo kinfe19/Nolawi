5e39b0fbb2075c2aeffc269e358f4f30da7f15b5
beka0205 <bekasnigussie19@gmail.com>
2022-08-05 22:00:59 +0300

    url add

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -4,7 +4,8 @@ from .views import *
 app_name = 'crudapp'
 urlpatterns = [
     path("", index, name="index"),
-    path("",list,name=list)
+    path("",list,name=list),
+    path("high",retrive,name="high")
     
 
 
